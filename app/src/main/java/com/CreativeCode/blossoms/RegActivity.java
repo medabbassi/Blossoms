4d22d84729240c799dcced6a116f5ebb98ca5d10
medex3 <mabbassix3@gmail.com>
2019-06-27 17:29:12 +0200

    Design Interfaces

--- app/src/main/java/com/CreativeCode/blossoms/RegActivity.java
+++ app/src/main/java/com/CreativeCode/blossoms/RegActivity.java
@@ -1,7 +1,8 @@
 package com.CreativeCode.blossoms;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class RegActivity extends AppCompatActivity {
 
